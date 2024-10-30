e202675244c5100df5fdb23ebd52ff4c2c8d2622
PavloOmelchenkokhai <p.v.omelchenko@student.khai.edu>
2024-10-30 18:23:29 +0200

    Hangman{5}stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -2,10 +2,27 @@ import random
 print ("HANGMAN")
 words = ['python', 'java', 'javascript', 'php']
 secret_word = random.choice(words)
-hint = secret_word[:3] + '-' * (len(secret_word) - 3)
-print(f"Guess the word {hint}: ", end="")
-guess = input(" ").strip().lower()
-if guess == secret_word:
- print("You survived!")
+attempts = 8
+guessed_letters = set()
+display_word = '-' * len(secret_word)
+
+while attempts > 0:
+    print("\n" + display_word)
+    guess = input("Input a letter: > ").strip().lower()
+
+    if guess in guessed_letters:
+        print("You've already tried that letter.")
+    elif guess in secret_word:
+        guessed_letters.add(guess)
+        display_word = ''.join([letter if letter in guessed_letters else '-' for letter in secret_word])
+        if display_word == secret_word:
+            print("\n" + display_word)
+            print("You survived!")
+            print("Thanks for playing!\nWe'll see how well you did in the next stage.")
+            break
+    else:
+        print("That letter doesn't appear in the word")
+        attempts -= 1
+        guessed_letters.add(guess)
 else:
- print("You lost!")
+    print("\nYou lost!")

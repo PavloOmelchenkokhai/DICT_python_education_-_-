d72c96368072541b23551595e80105b186587149
PavloOmelchenkokhai <p.v.omelchenko@student.khai.edu>
2024-10-30 16:43:44 +0200

    Hangman{2}stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,1 +1,8 @@
-print ("HANGMAN \nThe game will be available soon")
+print ("HANGMAN")
+
+secret_word = "python"
+guess = input("Guess the word: ").strip().lower()
+if guess == secret_word:
+ print("You survived!")
+else:
+ print("You lost!")

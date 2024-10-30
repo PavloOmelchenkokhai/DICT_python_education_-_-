bd3af2a7383426cc07f0a4515955ab7b8783af18
PavloOmelchenkokhai <p.v.omelchenko@student.khai.edu>
2024-10-30 17:12:12 +0200

    Hangman{4}stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -2,7 +2,9 @@ import random
 print ("HANGMAN")
 words = ['python', 'java', 'javascript', 'php']
 secret_word = random.choice(words)
-guess = input("Guess the word: ").strip().lower()
+hint = secret_word[:3] + '-' * (len(secret_word) - 3)
+print(f"Guess the word {hint}: ", end="")
+guess = input(" ").strip().lower()
 if guess == secret_word:
  print("You survived!")
 else:

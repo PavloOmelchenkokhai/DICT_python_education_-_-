fce0f2636531240b43c2db740e46548b560cffeb
PavloOmelchenkokhai <p.v.omelchenko@student.khai.edu>
2024-10-30 18:31:34 +0200

    Hangman{6}stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -11,18 +11,19 @@ while attempts > 0:
     guess = input("Input a letter: > ").strip().lower()
 
     if guess in guessed_letters:
-        print("You've already tried that letter.")
+        print("No improvements")
+        attempts -= 1
     elif guess in secret_word:
         guessed_letters.add(guess)
         display_word = ''.join([letter if letter in guessed_letters else '-' for letter in secret_word])
         if display_word == secret_word:
             print("\n" + display_word)
+            print("You guessed the word!")
             print("You survived!")
-            print("Thanks for playing!\nWe'll see how well you did in the next stage.")
             break
     else:
         print("That letter doesn't appear in the word")
         attempts -= 1
         guessed_letters.add(guess)
 else:
-    print("\nYou lost!")
+    print("You lost!")

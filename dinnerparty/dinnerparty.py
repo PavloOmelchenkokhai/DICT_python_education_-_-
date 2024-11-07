7d42f036ffe499b3d546dbfae485cc02c307c4e9
PavloOmelchenkokhai <p.v.omelchenko@student.khai.edu>
2024-11-07 18:14:00 +0200

    Dinnerparty{3}stage

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -1,3 +1,5 @@
+import random
+
 num_of_friends = int(input("Enter the number of friends joining (including you): "))
 
 if num_of_friends <= 0:
@@ -16,5 +18,26 @@ else:
 
     for name in friends:
         friends[name] = split_amount
+
+    choice = input('Do you want to use the "Who is lucky?" feature? Write Yes/No: ').strip().lower()
+
+    if choice == "yes":
+        lucky_one = random.choice(list(friends.keys()))
+        print(f"{lucky_one} is the lucky one!")
+
+        total_amount -= friends[lucky_one]
+
+        if num_of_friends == 1:
+            friends[lucky_one] = 0
+        else:
+            new_split = round(total_amount / (num_of_friends - 1), 2)
+            for name in friends:
+                if name != lucky_one:
+                    friends[name] = new_split
+                else:
+                    friends[name] = 0
+
+    else:
+        print("No one is going to be lucky")
 
     print(friends)

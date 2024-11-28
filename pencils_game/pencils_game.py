592e30699a7c24589a58484971c004550e8b723e
PavloOmelchenkokhai <p.v.omelchenko@student.khai.edu>
2024-11-28 15:52:51 +0200

    PensilsGame{3}Stage

--- pencils_game/pencils_game.py
+++ pencils_game/pencils_game.py
@@ -1,33 +1,44 @@
-pencil = input("How many pencils would you like to use: ")
-
 while True:
-    player1 = "Alex"
-    player2 = "Artem"
-    first_player = input(f"Who will be the first ({player1}, {player2}): ").capitalize()
-    if first_player not in [player1, player2]:
-        print(f"Choose between {player1} and {player2}")
+    pencil = input("How many pencils would you like to use: ")
+    if not pencil.isdigit():
+        print("The number of pencils should be numeric")
+    elif int(pencil) <= 0:
+        print("The number of pencils should be positive")
     else:
         pencil = int(pencil)
         break
 
-current_player  = first_player
-
-print("|" * pencil)
-print(f"{current_player} is going first!")
+player1 = "Alex"
+player2 = "Artem"
 
 while True:
-    try:
-        taken = int(input("> "))
-        if taken not in [1, 2, 3]:
-            print("Possible values: '1', '2' or '3'")
-        elif taken > pencil:
-            print("Too many pencils were taken")
-        else:
-            pencil -= taken
-            break
-    except ValueError:
-        print("Please input a number between 1 and 3.")
+    first_player = input(f"Who will be the first ({player1}, {player2}): ").capitalize()
+    if first_player not in [player1, player2]:
+        print(f"Choose between {player1} and {player2}")
+    else:
+        break
 
-    current_player = player1 if current_player == player2 else player2
+current_player = first_player
 
-    print("Game over!")
+while pencil > 0:
+    print("|" * pencil)
+    print(f"{current_player}'s turn!")
+
+    while True:
+        try:
+            taken = int(input("> "))
+            if taken not in [1, 2, 3]:
+                print("Possible values: '1', '2' or '3'")
+            elif taken > pencil:
+                print("Too many pencils were taken")
+            else:
+                pencil -= taken
+                break
+        except ValueError:
+            print("Please input a number between 1 and 3.")
+
+    if pencil > 0:
+        current_player = player1 if current_player == player2 else player2
+
+winner = player1 if current_player == player2 else player2
+print(f"{winner} won!")

d1da48898ae04c70096fd2cb999c789aae5847ec
PavloOmelchenkokhai <p.v.omelchenko@student.khai.edu>
2024-12-05 19:15:36 +0200

    Coffeemachine{3}stage

--- Coffeemachine/coffeemachine.py
+++ Coffeemachine/coffeemachine.py
@@ -1,11 +1,29 @@
+print("Write how many ml of water the coffee machine has:")
+water_available = int(input("> "))
+
+print("Write how many ml of milk the coffee machine has:")
+milk_available = int(input("> "))
+
+print("Write how many grams of coffee beans the coffee machine has:")
+coffee_beans_available = int(input("> "))
+
 print("Write how many cups of coffee you will need:")
-cups = int(input("> "))
+cups_needed = int(input("> "))
 
-water = cups * 200
-milk = cups * 50
-coffee_beans = cups * 15
+water_per_cup = 200
+milk_per_cup = 50
+coffee_beans_per_cup = 15
 
-print(f"\nFor {cups} cups of coffee you will need:")
-print(f"{water} ml of water")
-print(f"{milk} ml of milk")
-print(f"{coffee_beans} g of coffee beans")
+max_cups = min(
+    water_available // water_per_cup,
+    milk_available // milk_per_cup,
+    coffee_beans_available // coffee_beans_per_cup
+)
+
+if max_cups == cups_needed:
+    print("Yes, I can make that amount of coffee")
+elif max_cups > cups_needed:
+    extra_cups = max_cups - cups_needed
+    print(f"Yes, I can make that amount of coffee (and even {extra_cups} more than that)")
+else:
+    print(f"No, I can make only {max_cups} cups of coffee")
